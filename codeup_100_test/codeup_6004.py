c5af88da885a9932a907f9a6b9d1552681965e98
yoosoonil <y2k7654@gmail.com>
2022-07-11 16:25:46 +0900

    codeup_100_test

---
+++ codeup_100_test/codeup_6004.py
@@ -0,0 +1,23 @@
+#6004
+print("'Hello'")
+
+#6005
+print('"Hello World"')
+
+#6006
+print('"!@#$%^&*()\'')
+
+#6007
+print('"C:\\Download\\\'hello\'.py"')
+
+#6008
+print('print("Hello\\nWorld")')
+
+#6009
+c = input()
+print(c)
+
+#6010
+n = input()
+n = int(n)
+print(n)
---
+++ codeup_100_test/codeup_6011.py
@@ -0,0 +1,4 @@
+f=input()
+#f=float(f)
+print(f)
+# f는 표현한 수를 실수 그대로 표현.
